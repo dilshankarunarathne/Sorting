9bc5350a170c32c18fae77ada9a60367eb52183c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 10:54:15 +0530

    Update QuickSort.java

--- src/me/karunarathne/Sorting/QuickSort.java
+++ src/me/karunarathne/Sorting/QuickSort.java
@@ -1,5 +1,47 @@
 package me.karunarathne.Sorting;
 
-public class QuickSort {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class QuickSort {
+    public static void sort (Comparable [] a) {
+        shuffle (a) ;
+        sort (a, 0, a.length-1) ;
+    }
+
+    private static void sort (Comparable [] a, int lo, int hi) {
+        
+    }
+
+    private static boolean isLess (Comparable v,  Comparable w) {
+        return v.compareTo(w) < 0 ;
+    }
+
+    private static int partition (Comparable [] a, int lo, int hi) {
+        int i = lo ;
+        int j = hi+1 ;
+        while (true) {
+            while (isLess (a[++i], a[lo])) if (i==hi) break ;
+            while (isLess (a[lo], a[--j])) if (j==lo) break ;
+
+            if (i >= j) break ;
+            swap (a, i, j) ;
+        }
+
+        swap (a, lo, j) ;
+        return j ;
+    }
+
+    private static void swap (Comparable [] a, int i, int j) {
+        Comparable temp = a[i] ;
+        a[i] = a[j] ;
+        a[j] = temp ;
+    }
+
+    private static void shuffle (Comparable [] a) {
+        List<Comparable> list = Arrays.asList(a);
+        Collections.shuffle(list);
+        list.toArray(a);
+    }
 }
