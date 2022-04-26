1af5d21986beab32470a66ac222b1cbbb931bae3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 11:00:07 +0530

    Implemented Merge Sort

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import me.karunarathne.Sorting.BubbleSort;
 import me.karunarathne.Sorting.InsertionSort;
+import me.karunarathne.Sorting.MergeSort;
 import me.karunarathne.Sorting.SelectionSort;
 
 public class Main {
     public static void main(String[] args) {
         Integer [] numbers = {17, 79, 47, 12, 97, 65, 28, 47} ;
-        BubbleSort.sort(numbers);
+        MergeSort.sort(numbers);
         for (int i: numbers) System.out.println(i);
     }
 }
--- src/me/karunarathne/Sorting/MergeSort.java
+++ src/me/karunarathne/Sorting/MergeSort.java
@@ -1,7 +1,20 @@
 package me.karunarathne.Sorting;
 
 public class MergeSort {
-    public static void merge (Comparable [] a, Comparable [] aux, int lo, int mid, int hi) {
+    public static void  sort (Comparable [] a) {
+        Comparable [] aux = new Comparable [a.length] ;
+        sort (a, aux, 0, a.length-1) ;
+    }
+
+    private static void sort (Comparable [] a, Comparable [] aux, int lo, int hi) {
+        if (hi <= lo) return ;
+        int mid = lo+(hi-lo)/2 ;
+        sort (a, aux, lo, mid) ;
+        sort (a, aux, mid+1, hi) ;
+        merge (a, aux, lo, mid, hi) ;
+    }
+
+    private static void merge (Comparable [] a, Comparable [] aux, int lo, int mid, int hi) {
 //        precondition: a[lo..mid]         sorted
 //        assert isSorted (a, lo, mid) ;
 
