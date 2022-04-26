1fc3bda574ea1cda0039801d6d1b0dab6a68e79c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 10:40:18 +0530

    Fixes

--- src/me/karunarathne/Sorting/BubbleSort.java
+++ src/me/karunarathne/Sorting/BubbleSort.java
@@ -3,19 +3,20 @@ package me.karunarathne.Sorting;
 public class BubbleSort {
     public static void sort (Comparable [] a) {
         int N = a.length ;
-        Comparable temp = 0 ;
         for (int i=0; i<N; i++) {
             for (int j=1; j<N-i; j++) {
-                if (isLess(a[j], a[j-1])) {
-                    temp = a [j-1] ;
-                    a[j-1] = a[j] ;
-                    a[j] = temp ;
-                }
+                if (isLess(a[j], a[j-1])) swap (a, j, j-1) ;
             }
         }
     }
 
     private static boolean isLess (Comparable v,  Comparable w) {
         return v.compareTo(w) < 0 ;
+    }
+
+    private static void swap (Comparable [] a, int i, int j) {
+        Comparable temp = a[i] ;
+        a[i] = a[j] ;
+        a[j] = temp ;
     }
 }
