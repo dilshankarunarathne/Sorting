6b7d65bc4b85e777507aee1d6e735be459aa6ed7
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 11:23:27 +0530

    Fixes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import me.karunarathne.Sorting.*;
 public class Main {
     public static void main(String[] args) {
         Integer [] numbers = {17, 79, 47, 12, 97, 65, 28, 47} ;
-        
+
 //        InsertionSort.sort (numbers) ;
 //        SelectionSort.sort (numbers) ;
 //        BubbleSort.sort (numbers) ;
