ebc5882339fa508fb4638e751f37006b4de6e3d4
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 11:17:34 +0530

    Fixes

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import me.karunarathne.Sorting.*;
 public class Main {
     public static void main(String[] args) {
         Integer [] numbers = {17, 79, 47, 12, 97, 65, 28, 47} ;
-        InsertionSort.sort (numbers) ;
+        
+//        InsertionSort.sort (numbers) ;
 //        SelectionSort.sort (numbers) ;
 //        BubbleSort.sort (numbers) ;
-//        MergeSort.sort (numbers) ;
+        MergeSort.sort (numbers) ;
 //        QuickSort.sort (numbers) ;
+
         for (int i: numbers) System.out.print(i + " ");
     }
 }
