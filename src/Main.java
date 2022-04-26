e298b7308374eac49fe85c5222304c92e5b95952
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 11:03:14 +0530

    Bytecode Output

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import me.karunarathne.Sorting.BubbleSort;
-import me.karunarathne.Sorting.InsertionSort;
-import me.karunarathne.Sorting.MergeSort;
-import me.karunarathne.Sorting.SelectionSort;
+import me.karunarathne.Sorting.*;
 
 public class Main {
     public static void main(String[] args) {
         Integer [] numbers = {17, 79, 47, 12, 97, 65, 28, 47} ;
-        MergeSort.sort(numbers);
+        QuickSort.sort(numbers);
         for (int i: numbers) System.out.println(i);
     }
 }
--- src/me/karunarathne/Sorting/MergeSort.java
+++ src/me/karunarathne/Sorting/MergeSort.java
@@ -8,7 +8,7 @@ public class MergeSort {
 
     private static void sort (Comparable [] a, Comparable [] aux, int lo, int hi) {
         if (hi <= lo) return ;
-        int mid = lo+(hi-lo)/2 ;
+        int mid = lo + ((hi-lo)/2) ;
         sort (a, aux, lo, mid) ;
         sort (a, aux, mid+1, hi) ;
         merge (a, aux, lo, mid, hi) ;
