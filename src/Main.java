dd0bbf07ab120a9af6530362124e8bc28d1c1768
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 10:38:04 +0530

    Finished BubbleSort

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import me.karunarathne.Sorting.BubbleSort;
 import me.karunarathne.Sorting.InsertionSort;
 import me.karunarathne.Sorting.SelectionSort;
 
 public class Main {
     public static void main(String[] args) {
         Integer [] numbers = {17, 79, 47, 12, 97, 65, 28, 47} ;
-        SelectionSort.sort(numbers);
+        BubbleSort.sort(numbers);
         for (int i: numbers) System.out.println(i);
     }
 }
--- src/me/karunarathne/Sorting/BubbleSort.java
+++ src/me/karunarathne/Sorting/BubbleSort.java
@@ -3,15 +3,19 @@ package me.karunarathne.Sorting;
 public class BubbleSort {
     public static void sort (Comparable [] a) {
         int N = a.length ;
-        int temp = 0 ;
+        Comparable temp = 0 ;
         for (int i=0; i<N; i++) {
             for (int j=1; j<N-i; j++) {
-                if (a[j-1] > a[j]) {
+                if (isLess(a[j], a[j-1])) {
                     temp = a [j-1] ;
                     a[j-1] = a[j] ;
                     a[j] = temp ;
                 }
             }
         }
+    }
+
+    private static boolean isLess (Comparable v,  Comparable w) {
+        return v.compareTo(w) < 0 ;
     }
 }
