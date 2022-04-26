ecaea21d2d9c18b4da12dfe8004b0ec9c669e929
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 10:13:25 +0530

    init

---
+++ src/me/karunarathne/Sorting/BubbleSort.java
@@ -0,0 +1,5 @@
+package me.karunarathne.Sorting;
+
+public class BubbleSort {
+
+}
---
+++ src/me/karunarathne/Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package me.karunarathne.Sorting;
+
+public class SelectionSort {
+    public static void sort (Comparable[] a) {
+        int N = a.length ;
+        for (int i=0; i<N; i++) {
+            int min = i ;
+            for (int j=i+1; j<N; j++) {
+                if (isLess (a[j], a[min])) min = j ;
+            }
+            swap (a, i, min) ;
+        }
+    }
+
+    private static boolean isLess (Comparable v,  Comparable w) {
+        return v.compareTo(w) < 0 ;
+    }
+
+    private static void swap (Comparable [] a, int i, int j) {
+        Comparable temp = a[i] ;
+        a[i] = a[j] ;
+        a[j] = temp ;
+    }
+}
