f014284a8248536d13f6aa3dd3076db1b7d89084
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 10:20:22 +0530

    Added sorting algos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import me.karunarathne.Sorting.InsertionSort;
+import me.karunarathne.Sorting.SelectionSort;
+
 public class Main {
     public static void main(String[] args) {
-
+        Integer [] numbers = {17, 79, 47, 12, 97, 65, 28, 47} ;
+        SelectionSort.sort(numbers);
+        for (int i: numbers) System.out.println(i);
     }
 }
--- src/me/karunarathne/Sorting/InsertionSort.java
+++ src/me/karunarathne/Sorting/InsertionSort.java
@@ -1,5 +1,23 @@
 package me.karunarathne.Sorting;
 
 public class InsertionSort {
-    
+    public static void sort (Comparable [] a) {
+        int N = a.length ;
+        for (int i=0; i<N; i++) {
+            for (int j=i; j>0; j--) {
+                if (isLess(a[j], a[j-1])) swap(a, j, j-1) ;
+                else break ;
+            }
+        }
+    }
+
+    private static boolean isLess (Comparable v,  Comparable w) {
+        return v.compareTo(w) < 0 ;
+    }
+
+    private static void swap (Comparable [] a, int i, int j) {
+        Comparable temp = a[i] ;
+        a[i] = a[j] ;
+        a[j] = temp ;
+    }
 }
