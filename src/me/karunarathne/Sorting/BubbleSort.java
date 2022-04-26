141b1a3d3ebc28383bf10b3c0de3c17c1aa01dcf
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-26 10:36:32 +0530

    Update BubbleSort.java

--- src/me/karunarathne/Sorting/BubbleSort.java
+++ src/me/karunarathne/Sorting/BubbleSort.java
@@ -1,5 +1,17 @@
 package me.karunarathne.Sorting;
 
 public class BubbleSort {
-
+    public static void sort (Comparable [] a) {
+        int N = a.length ;
+        int temp = 0 ;
+        for (int i=0; i<N; i++) {
+            for (int j=1; j<N-i; j++) {
+                if (a[j-1] > a[j]) {
+                    temp = a [j-1] ;
+                    a[j-1] = a[j] ;
+                    a[j] = temp ;
+                }
+            }
+        }
+    }
 }
